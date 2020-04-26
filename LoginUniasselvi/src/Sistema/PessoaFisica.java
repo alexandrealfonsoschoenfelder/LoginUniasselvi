99cc3ebb9fb2cecd6324cbf61fa42109f8f9ea63
Alexandre Alfonso Schoenfelder <alexandrealfonsoschoenfelder@gmail.com>
2020-04-26 11:57:28 -0300

    Versão final do Sistema

---
+++ LoginUniasselvi/src/Sistema/PessoaFisica.java
@@ -0,0 +1,20 @@
+package Sistema;
+
+public abstract class PessoaFisica extends Pessoa {
+
+	private String cpf;
+	
+	public PessoaFisica() {
+		super();
+		this.cpf = "";
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+}
